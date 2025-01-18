0d538b7ffe0ce4fb89307c309dcd0f4b946a3c6b
Shivanijadav05 <shivanihjadav@gmail.com>
2025-01-18 19:37:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const PORT=process.env.PORT || 4000;
 
 
 
-app.use(bodyParser.json());
+
 app.use(cors({
   origin: [`https://deploy-mern-app-uiii.vercel.app`], // Frontend URL
   methods: ['GET', 'POST'], // Allowed HTTP methods
@@ -21,18 +21,7 @@ app.use(cors({
   optionSuccessStatus:200
 }));
 
- app.use((req, res, next) => {
-res.setHeader('Access-Control-Allow-Origin', 'https://deploy-mern-app-uiii.vercel.app');
-res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-res.setHeader('Access-Control-Allow-Credentials', 'true'); // Optional: For cookies/auth
-    });
- app.options('/auth/login', (req, res) => {
-      res.header('Access-Control-Allow-Origin', 'https://deploy-mern-app-uiii.vercel.app');
-      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-      res.send();
-    });
+ app.use(bodyParser.json());
 
 
 
