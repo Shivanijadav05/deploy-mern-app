63024d43a93d85525ad0f7c95e708d85cbb0990d
Shivanijadav05 <shivanihjadav@gmail.com>
2025-01-18 19:45:35 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,7 @@ function App() {
       <Route path='/' element={<Navigate to={'/login'}/>}/>
       <Route path='/login' element={<Login/>}/>
       <Route path='/signup' element={<Signup/>}></Route>
-      <Route path='/home' element={<Home/>}/>
+       <Route path='/home' element={<PrivateRoute element={<Home />} />} />
       </Routes>
     </div>
   );
