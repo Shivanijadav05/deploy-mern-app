6f7e70d7c749cc168df20f1d4704946e4d2e7090
Shivanijadav05 <shivanihjadav@gmail.com>
2025-01-18 15:35:56 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,18 @@ import './App.css';
 import Login from './pages/login';
 import Signup from './pages/signup';
 import Home from './pages/home';
+import { useState } from 'react';
 import {Navigate,Route,Routes} from 'react-router-dom';
-
+import RefrshHandler from './RefrshHandler';
 function App() {
+   const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const PrivateRoute = ({ element }) => {
+    return isAuthenticated ? element : <Navigate to="/login" />
+  }
   return (
     <div className="App">
+    <RefrshHandler setIsAuthenticated={setIsAuthenticated} /
       <Routes>
       <Route path='/' element={<Navigate to={'/login'}/>}/>
       <Route path='/login' element={<Login/>}/>
